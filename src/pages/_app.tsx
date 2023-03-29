11e8f9dcb56ec1bd1225c98ec622e4a6665f1cb5
Rafael de Jesus <rrrrebolledohdz@gmail.com>
2023-03-29 11:53:39 -0600

    config tailwindcss, Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Layout from '@/components/Layout';
+
+import { Poppins } from 'next/font/google';
+const poppins = Poppins({
+  weight: ['200', '300', '400', '500', '600', '700', '800'],
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${poppins.variable} font-sans`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
+  );
 }
