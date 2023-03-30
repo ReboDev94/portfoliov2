8a6795080113df7e703502654147fcebf0bf8c50
Rafael de Jesus <rrrrebolledohdz@gmail.com>
2023-03-30 16:22:43 -0600

    added font awesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/Layout';
-
 import { Poppins } from 'next/font/google';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
 const poppins = Poppins({
   weight: ['200', '300', '400', '500', '600', '700', '800'],
   subsets: ['latin'],
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
+
 export default function Home() {
   return (
     <section className="flex">
       <div className="w-20 py-5 h-screen border-r border-r-gray-200 flex flex-col items-center justify-between">
         <h1 className="text-gray-700 font-bold text-xl">
-          <span>{ '< R' }</span>
-          <span className="text-red-500">{ 'J ' }</span>
-          <span>{ '/>' }</span>
+          <span>{'< R'}</span>
+          <span className="text-red-500">{'J '}</span>
+          <span>{'/>'}</span>
         </h1>
         <div>
-            social
+          <FontAwesomeIcon icon={faGithubSquare} className="text-red-900" style={{ fontSize: 30 }} />
         </div>
       </div>
       <div className="flex-1">2</div>
