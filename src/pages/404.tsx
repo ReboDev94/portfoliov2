5d38bbf865a2ed4cd638e92dc187b905af6be70c
Rafael Rebolledo <rrrrebolledohdz@gmail.com>
2023-05-18 16:11:41 -0600

    page notfound

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from '../components/Button';
+import Link from 'next/link';
+
+const Custom404 = () => {
+    return (
+        <div className='min-h-[calc(100vh-4rem)] grid place-content-center text-center gap-4'>
+            <h1 className='font-semibold text-3xl'><span className='text-red-600'>A</span>re you lost?</h1>
+            <span>It looks like the page you are looking for does not exist</span>
+            <Link href="/">
+                <Button>Go Home</Button>
+            </Link>
+        </div>
+    )
+}
+
+export default Custom404
